7dc06253c8fda899daaece395b5d469ec3c8b231
KOITT <KOITT@KOITT-PC04>
2017-12-27 17:03:58 +0900

    배열 127쪽까지

--- ch04/src/control/loop/While.java
+++ ch04/src/control/loop/While.java
@@ -2,10 +2,10 @@ package control.loop;
 
 public class While {	
 	public static void main(String[] args) {
-		int i =1; 
+		int i = 1; 
 		
 		while (i <= 10){
-			System.out.print(i++ = " ");
+			System.out.print(i++ + " ");
 		}
 		System.out.println();
 	}
