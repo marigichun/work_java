8611c4d51c60e98c1c68bd051024d67ccf0368b0
김상일 <@59.15.151.220>
2018-01-03 21:50:44 +0900

    추가

--- ch05/src/com/koitt/java/class04/Owner.java
+++ ch05/src/com/koitt/java/class04/Owner.java
@@ -3,17 +3,46 @@ package com.koitt.java.class04;
 import obj.constructor.Car;
 
 public class Owner {
-	private String name;		//차주의 이름
-	private String address;		//차주 주소
-	private Car car1 ;			//소유한 자동차
+		private String name;
+		private Car car;
+		
 	
-	public Owner (String name, String address, Car car1) {
-		this.name = name;
-		this.address = address;
-		this.car1 = car1;
-	}
-	public void introCar() {
-		System.out.print(name + "의 자동차 브랜드 이름은");
-		System.out.println(car1.getBrandName()+ "이고, 최고 속도는" + car1.getMaxSpeed() +"km/h 입니다.");		
+		public Owner(String name, Car car) {
+			this.name = name;
+			this.car = car;
+		}
+
+	
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Car getCar() {
+			return car;
+		}
+
+		public void setCar(Car car) {
+			this.car = car;
+		}
+
+		public void introCar() {
+			System.out.print(this.name + "의 자동차의 브랜드 이름은");
+			System.out.print(car.getBrandName() + " 이고, 최고속도는 ");
+			System.out.println(car.getMaxSpeed() + "km/h 입니다.");
+			
 		}
 }
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
--- ch05/src/com/koitt/java/class04/TestDrive.java
+++ ch05/src/com/koitt/java/class04/TestDrive.java
@@ -1,19 +1,15 @@
 package com.koitt.java.class04;
 
-
+import obj.constructor.Car;
 
 public class TestDrive {
 	public static void main(String[] args) {
+		Car gene = new Car("현대 제네시스", 220);
+		Owner jisoo = new Owner("김지수", Genesis);
+		jisoo.introCar();
 		
-		Car str = new Car("스팅어", "현대");
-		Owner jiho = new Owner("지호", "서울", "스팅어");
-		jiho.introCar();
-		
-		
-		Car a = new Car("지수", "재규어");
-		Owner gildong = new Owner("홍길동", "대구", "소나타");
-		gildong.introCar();
+		Car benz = new Car("Benz", 220);
+		Owner sooji = new Owner("김수지", Benz);
+		sooji.introCar();
 	}
 }
-
-
