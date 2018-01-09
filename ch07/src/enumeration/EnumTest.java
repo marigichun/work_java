01e7633f583cac4233e25bccdd71aa64f112cfce
KOITT <KOITT@KOITT-PC04>
2018-01-09 09:07:57 +0900

    7장 예제

---
+++ ch07/src/enumeration/EnumTest.java
@@ -0,0 +1,20 @@
+package enumeration;
+
+public class EnumTest {
+	public enum pl {c, cpp, java, csharp};
+	
+	public static void main(String[] args, String p) {
+		pl clang = pl.c;				//정의한 열거 유형은 한나의 자료 유형으로 이용
+		System.out.println(clang);		//저장된 상수 문자열이 출력
+		
+		clang = pl.csharp;
+		switch(clang) {
+		case csharp:
+			System.out.println(clang + ": C# 언어 ");
+		}
+		
+		for (pl p : pl.values());
+			System.out.print(p + " ");
+		System.out.println();
+	}
+}
